39a54bb34a3f65e502f1b246317fee0f5cbb309b
Isaac Huffman <122467839+isaachuffman37@users.noreply.github.com>
2024-06-05 23:26:37 -0600

    finished it

---
+++ js/review.js
@@ -0,0 +1,4 @@
+const thanksButton = document.querySelector(".buy-button");
+thanksButton.addEventListener('click', () => {
+    window.location.href = '../thanks/thanks.html'; // Replace with your desired URL
+});
